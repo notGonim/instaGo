74e1edd9e0fb59c7d08fe9c867774f3f1469bd3b
notGonim <mhmodgonim96@gmail.com>
2021-05-26 04:40:08 +0200

    createContext in firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,25 @@
+
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+//here i want to import seed file 
+import { seedDatabase } from '../seed.js'
+
+
+const config = {
+    apiKey: "AIzaSyDLmYJzMpm67dA94LC77XBUrbysC7DmF7A",
+    authDomain: "instago-4e98b.firebaseapp.com",
+    projectId: "instago-4e98b",
+    storageBucket: "instago-4e98b.appspot.com",
+    messagingSenderId: "3782018195",
+    appId: "1:3782018195:web:839c4fe06405a294f2b966"
+}
+
+const firebase = Firebase.initializeApp(config)
+const { FieldValue } = Firebase.firestore
+
+
+//here is where i wanna call the seed file (only once)
+seedDatabase(firebase)
+export { firebase, FieldValue }
