02e8b59e063576fe11b331b4be2d86cc542cc053
notGonim <mhmodgonim96@gmail.com>
2021-05-26 05:09:21 +0200

    Start working on Login Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from './constants/routes'
+
 
 const login = lazy(() => import('./pages/Login'))
 
@@ -10,7 +12,7 @@ function App() {
     <Router>
       <Suspense fallback={<p>Loading ...</p>}>
         <Switch>
-          <Route path='/login' exact component={login} />
+          <Route path={ROUTES.LOGIN} exact component={login} />
         </Switch>
       </Suspense>
     </Router>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
-
-
+import React, { useContext, useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
+import FirebaseContext from '../context/firebase'
 
 export default function Login() {
+
+    const history = useHistory()
+    const { firebase } = useContext(FirebaseContext)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const isInvalid = password === '' || email === ''
+
+
+    const handleLogin = () => {
+
+    }
+
+    useEffect(() => {
+        document.title = 'Login - InstaGo'
+    }, [])
+
     return (
         <div>
             <p>Iam in the login page</p>
