e9a8a25dc7314e3202ceb32535926c3c9a31d4a1
notGonim <mhmodgonim96@gmail.com>
2021-05-29 10:48:59 +0200

    ADDED the user component content

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import useUser from '../hooks/use-user'
 import Suggestion from './suggestion'
-import User from './user'
-
+import { User } from './user'
 export default function Sidebar() {
 
 
-    const { user: { fullName, username, userId } } = useUser()
+    const { user: { fullname, userId, username } } = useUser()
+
     return (
         <div>
-            <User />
-            <Suggestion />
+            <User username={username} fullname={fullname} />
+            <Suggestion userId={userId} />
         </div>
 
     )
--- src/components/user.js
+++ src/components/user.js
@@ -1,10 +1,38 @@
+import { memo } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import { Link } from 'react-router-dom'
+
+
+
+
+
 import React from 'react'
 
-export default function User(){
+export const User = ({ username, fullname }) => {
     return (
-        <div className="p-4">
-            <h1>User</h1>
-        </div>
+        <>
+            {
+                !username || !fullname ? (
+                    <Skeleton count={1} height={61} />
+                ) : (
+                    <Link to={`/p/${username}`} className='grid grid-cols-4 gap-4 mb-6 items-center ' >
+                        <div className="flex items-center justify-between col-span-1">
+                            <img
+                                className="rounded-full h-8 w-8 flex"
+                                src={`images/avatars/karl.jpg`}
+                                alt={`${username} profile`}
 
+                            />
+                        </div>
+                        <div className="col-span-3">
+                            <p className="font-bold text-sm">{username}</p>
+                            <p className="text-sm">{fullname}</p>
+                        </div>
+                    </Link>
+
+                )
+
+            }
+        </>
     )
 }
