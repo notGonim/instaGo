99a2a29778e97b8a0a4faae4f66487def93bd373
notGonim <mhmodgonim96@gmail.com>
2021-05-30 15:24:06 +0200

    Working on the suggestion component

--- src/components/suggestion.js
+++ src/components/suggestion.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Skeleton from 'react-loading-skeleton'
 
-export default function Suggestion(){
+export default function Suggestion({ userId }) {
+
+    const [profiles, setProfiles] = useState(null)
+
+    useEffect(() => {
+
+    }, [userId])
     return (
-        <div>
-            <h1>Suggestion</h1>
-        </div>
+
+        <>
+            {
+                !profiles ? (
+                    <Skeleton count={1} height={150} className="mt-5" />
+                ) : profiles.length > 0 ? (
+                    <div className="rounded flex flex-col">
+                        <div className="text-sm flex items-center align-items justify-between mb-2">
+                            <p className="font-bold text-gray-base">Suggestion</p>
+                        </div>
+                    </div>
+                ) : (null)
+
+            }
+        </>
 
     )
 }
