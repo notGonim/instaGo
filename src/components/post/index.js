9ec7c1592a070c8a55b4424d19f6fafa8fcf9993
notGonim <mhmodgonim96@gmail.com>
2021-06-15 13:15:01 +0200

    Show Comments
    - Showed the comments that photo has
    - Showed the user that wrote that comment
    - Show the dateCreated for that photo

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -3,15 +3,15 @@ import Actions from "./actions";
 import Footer from "./footer";
 import Header from "./header";
 import Image from "./image";
+import Comments from '../post/comment'
 
 
-
-export default function Post({ username, docId, likes, userLikedPhoto, src, caption }) {
+export default function Post({ username, dateCreated, docId, likes, comments, userLikedPhoto, src, caption }) {
 
 
     const commentInput = useRef(null)
     const handleFocus = () => commentInput.current.focus()
-
+console.log(dateCreated)
     return (
 
         <div className="rounded col-span-4 mb-8 border bg-white  border-gray-primary ">
@@ -19,8 +19,11 @@ export default function Post({ username, docId, likes, userLikedPhoto, src, capt
             <Image src={src} caption={caption} />
             <Actions docId={docId} totalLikes={likes.length} likedPhoto={userLikedPhoto} handleFocus={handleFocus} />
             <Footer caption={caption} username={username} />
+            <Comments docId={docId} posted={dateCreated} commentInput={commentInput}  comments={comments} />
         </div>
     )
 
 
 }
+
+
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -6,7 +6,6 @@ import Post from './post'
 export default function Timeline() {
 
     const { photos } = usePhotos()
-
     return (
         <div className="container col-span-2">
             {
@@ -20,6 +19,8 @@ export default function Timeline() {
                     (photos.map((content) =>
                         <Post key={content.docId} docId={content.docId}
                             caption={content.caption}
+                            comments={content.comments}
+                            dateCreated={content.dateCreated}
                             likes={content.likes} userLikedPhoto={content.userLikedPhoto} username={content.username} src={content.imageSrc} />)
                     ) : (<p className="text-center text-2xl">Follow people to see photos</p>)
             }
