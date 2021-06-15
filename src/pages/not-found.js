464e4f6f992ee0f807b6889bf95237ee07bc90cc
notGonim <mhmodgonim96@gmail.com>
2021-06-15 10:36:07 +0200

    ADDED the header to not-found page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-
+import Header  from '../components/header'
 
 export default function NotFound() {
 
@@ -7,9 +7,10 @@ export default function NotFound() {
     useEffect(() => {
         document.title = 'Not Found - InstaGo'
     }, [])
-    
+
     return (
         <div className=" bg-gray-background">
+            <Header />
             <div className="mx-auth max-w-screen-lg">
                 <p className="text-center text-2xl">Not Found!</p>
             </div>
